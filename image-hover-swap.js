4dd412a82b7a7bd1202503dbd1040fb49d04c994
Andrew Gehman <andrew.gehman@gmail.com>
2014-05-16 16:47:52 -0400

    Image Hover Swap
    Simple jQuery image swap on hover script

---
+++ image-hover-swap.js
@@ -0,0 +1,29 @@
+// About this file:  
+// This jQuery script flips from one image to another on mouse  
+// over and flips back to the original image on mouse out.
+//
+// How to use this file:
+//
+// ** Add a link to this .js file in your html document. 
+// 1. Append '_first' (without the quotes) to the end of the original image's file name.
+// 2. Append '_second' (without the quotes) to the end of the other image's file name (this is the image 
+// 	  you wish to appear on mouse over).
+// 3. add the attribute of class="swap" to the html of any images you with to "swap"
+// 
+// This will work with any/all images on the page witj the swap class & appropriate image file names. 
+
+$(document).ready(function(){ 	
+					   
+	$("img.swap").on("mouseenter", function(){	
+		
+		$(this).attr('src', $(this).attr('src').replace('_first', '_second'));
+		
+		});
+		
+	$("img.swap").on("mouseleave", function(){
+
+		$(this).attr('src', $(this).attr('src').replace('_second', '_first'));
+		
+		});	
+	
+});
